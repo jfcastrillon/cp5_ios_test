823f39c9d6a57087417099c72268a5f430e32d10
jcannon <jcannon@wellsky.com>
2010-06-20 01:46:00 +0000

    Copy address info from the primary address to the CPMResource base class fields
    git-svn-id: https://svn.bowmansystems.net/cp5_ios/trunk@19 b6d98a84-8402-493f-99b4-82035377a125

--- CommunityPointMobile/Classes/CPMResourceDetail.m
+++ CommunityPointMobile/Classes/CPMResourceDetail.m
@@ -27,6 +27,14 @@
 		NSDictionary* primaryAddressDict = nullFix([addressDict objectForKey:@"primary"]);
 		CPMProviderAddress *tempPrimaryAddress = [[CPMProviderAddress alloc] initFromJsonDictionary: primaryAddressDict];
 		self.primaryAddress = tempPrimaryAddress;
+		
+		// Copy these fields over to fill in the super class fields
+		/*self.address1 = [tempPrimaryAddress line1];
+		self.address2 = [tempPrimaryAddress line2];
+		self.city = [tempPrimaryAddress city];
+		self.state = [tempPrimaryAddress province];
+		self.zipcode = [NSDecimalNumber decimalNumberWithString: [tempPrimaryAddress postalcode]];*/
+		
 		[tempPrimaryAddress release];
 		
 		
