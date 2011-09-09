bed4b6cec442c645d3727f53f513602098e080bd
mbaker <mbaker@wellsky.com>
2011-09-09 21:30:35 +0000

    Merging r203 from trunkHide common searches when switching from other tabs back to the Home tab.
    (Merging r203 from trunk).
    
    
    git-svn-id: https://svn.bowmansystems.net/cp5_ios/branches/switchboard_miami@260 b6d98a84-8402-493f-99b4-82035377a125

---
+++ CommunityPointMobile/CPTabBarDelegate.m
@@ -0,0 +1,23 @@
+//
+//  CPTabBarDelegate.m
+//  CommunityPointMobile
+//
+//  Created by John Cannon on 5/26/11.
+//  Copyright 2011 Bowman Systems, LLC. All rights reserved.
+//
+
+#import "CPTabBarDelegate.h"
+
+
+
+@implementation CPTabBarDelegate
+
+- (void) tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController {
+    if (viewController == [[tabBarController viewControllers] objectAtIndex:HOME_VIEW_CONTROLLER_INDEX]) {
+        UINavigationController* homeViewNavController = (UINavigationController*) viewController;
+        homeViewNavController.navigationBar.hidden = YES;
+        [homeViewNavController popToRootViewControllerAnimated:NO];
+    }
+}
+
+@end
